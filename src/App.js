50843b808aea4450d13dc549a8d3e7a4a4278cb0
Dedpool1998 <msmsmanishkumar64@gmail.com>
2022-05-16 22:30:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Answer1 from './components/Answer1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      
+        <Hello></Hello>
+        <Answer1></Answer1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Answer1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Answer1 = () =>{
+
+    return <div>Hello from Answer1</div>;
+}
+
+export default Answer1;
